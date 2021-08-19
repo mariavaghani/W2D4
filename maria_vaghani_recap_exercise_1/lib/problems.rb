7c1eb4b3b7a8d5e6c4bcd7ec43719a77ae64deb7
mariavaghani <maria.vaghani@gmail.com>
2021-08-19 10:40:32 -0400

    pass all specs for recap 1

--- maria_vaghani_recap_exercise_1/lib/problems.rb
+++ maria_vaghani_recap_exercise_1/lib/problems.rb
@@ -47,16 +47,20 @@ end
 def find_bigrams(str, bigrams)
     result = []
     bigrams.each do |bigram|
-        bigram << result if 
+        test = str.split(bigram)
+        result << bigram if test.length > 1 || test[0].length != str.length
     end
+    result
 end
 
-str = "ha"
-another_str = "to the moon and back"
+# str = "ha"
+# another_str = "to the moon and back"
 
-p another_str.split(str) 
-p find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
-p find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
+# p another_str.split(str) 
+# p find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
+# p find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
+
+
 class Hash
     # Write a method, Hash#my_select, that takes in an optional proc argument
     # The method should return a new hash containing the key-value pairs that return
@@ -65,16 +69,18 @@ class Hash
     #
     # Examples:
     #
-    # hash_1 = {x: 7, y: 1, z: 8}
-    # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
-    #
-    # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
-    # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
-    # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
+        prc ||= Proc.new { |k, v| k == v }
 
+        self.select { |k, v| prc.call(k,v)}
     end
 end
+# hash_1 = {x: 7, y: 1, z: 8}
+# p hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
+
+# hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
+# p hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
+# p hash_2.my_select                            # => {4=>4}
 
 class String
     # Write a method, String#substrings, that takes in a optional length argument
@@ -83,23 +89,42 @@ class String
     #
     # Examples:
     #
-    # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
-    # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
-
+        result = []
+        (0...self.length).each do |start_idx|
+            (start_idx...self.length).each do |end_idx|
+                result << self[start_idx..end_idx]
+            end
+        end
+        if length
+            result.select { |ele| ele.length == length }
+        else
+            result
+        end
     end
-
-
+    
+    
     # Write a method, String#caesar_cipher, that takes in an a number.
     # The method should return a new string where each char of the original string is shifted
     # the given number of times in the alphabet.
     #
     # Examples:
     #
-    # "apple".caesar_cipher(1)    #=> "bqqmf"
-    # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
-    # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
+        alpha = ("a".."z").to_a
+        new_str = ""
 
+        self.each_char do |char|
+            old_idx = alpha.index(char)
+            new_idx = (old_idx + num) % alpha.length
+            new_str += alpha[new_idx]
+        end
+        new_str
     end
 end
+
+# p "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
+# p "cats".substrings(2)  # => ["ca", "at", "ts"]
+# p "apple".caesar_cipher(1)    #=> "bqqmf"
+# p "bootcamp".caesar_cipher(2) #=> "dqqvecor"
+# p "zebra".caesar_cipher(4)    #=> "difve"
